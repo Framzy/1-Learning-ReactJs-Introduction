5c799ba70386b7f9369b9660b334d0c578e8d636
Framzy <fardenramzy97@gmail.com>
2025-10-10 13:16:04 +0700

    Update Task with Array State

---
+++ src/task/Task.jsx
@@ -0,0 +1,49 @@
+import { useImmer } from "use-immer";
+import { useState } from "react";
+
+export default function Task() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useImmer([]);
+
+  function handleChange(e) {
+    setItem(e.target.value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    if (!item) {
+      return;
+    }
+
+    setItems((items) => {
+      items.push(item);
+    });
+
+    setItem("");
+  }
+
+  function handleRemoveItem(value) {
+    setItems((items) => {
+      const newItems = items.filter((item) => item !== value);
+      return newItems;
+    });
+  }
+
+  return (
+    <div>
+      <h1>Create Task</h1>
+      <form action="#">
+        <input value={item} onChange={handleChange} />
+        <button onClick={handleClick}>Add</button>
+        <button onClick={() => handleRemoveItem(item)}>Del</button>
+      </form>
+      <h1>List Task</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
