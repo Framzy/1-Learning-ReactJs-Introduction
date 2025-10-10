2c5cca0f120e3dd7f84dc570a2ee82259aa70be2
Framzy <fardenramzy97@gmail.com>
2025-10-10 09:27:36 +0700

    Add new folder contact

---
+++ src/contact/ContactForm.jsx
@@ -0,0 +1,10 @@
+import "./Contact.css";
+export default function ContactForm() {
+  return (
+    <div>
+      <div className="header">
+        <h1 className="title">Contact</h1>
+      </div>
+    </div>
+  );
+}
